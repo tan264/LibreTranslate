6f8da51e5244b50aa01a1a71c15d6d9b515126c4
Đặng Tấn <tandang264@gmail.com>
2024-09-09 14:26:24 +0700

    test

--- libretranslate/init.py
+++ libretranslate/init.py
@@ -8,6 +8,7 @@ import libretranslate.language
 
 def boot(load_only=None, update_models=False, install_models=False):
     try:
+        print("Installing models in local directory")
         package_path_en_vi = pathlib.Path("translate-en_vi-1_0.argosmodel") 
         package_path_vi_en = pathlib.Path("translate-vi_en-1_0.argosmodel") 
         package.install_from_path(package_path_en_vi)
